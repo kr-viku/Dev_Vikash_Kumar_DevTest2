7a5b530598347aa178978b5cf5e6b83879bfe379
er.vikashs4@gmail.com <vikash@refactor.academy>
2025-01-14 16:10:23 +0530

    upload video ui has done

--- js/main.js
+++ js/main.js
@@ -109,3 +109,34 @@ $(document).click(function () {
 $(".btn-row").click(function (e) {
   e.stopPropagation();
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  var video = document.getElementById("myVideo");
+  video.currentTime = 0; // Reset video to the beginning
+  video.play();
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+  document.getElementById("myVideo").pause();
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementById("myVideo").pause();
+  }
+};
