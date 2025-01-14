f8ad431fc2c10e27b910d8a645399e2e751f4690
er.vikashs4@gmail.com <vikash@refactor.academy>
2025-01-14 19:36:33 +0530

    country dropdown implemented

--- js/country_list.js
+++ js/country_list.js
@@ -1,11 +1,20 @@
 let country_list = ["Australia", "India", "USA"];
+const select = document.getElementById("country");
+const label = document.querySelector(".country-label span");
 
-var select = document.getElementById("country");
-
-for (var i = 0; i < country_list.length; i++) {
-  var opt = country_list[i];
-  var el = document.createElement("option");
-  el.textContent = opt;
-  el.value = opt;
+// Populate the select element
+for (let i = 0; i < country_list.length; i++) {
+  const el = document.createElement("option");
+  el.textContent = country_list[i];
+  el.value = country_list[i];
   select.appendChild(el);
 }
+
+// Add event listener for changes
+select.addEventListener("change", () => {
+  if (select.value) {
+    label.classList.add("active");
+  } else {
+    label.classList.remove("active");
+  }
+});
